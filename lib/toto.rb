66196d12901da9b304bcdd1b126a21c8804082bc
Manuel Hotz <manuel.hotz@enplotz.de>
2012-03-11 18:54:48 +0100

    add a category method

--- lib/toto.rb
+++ lib/toto.rb
@@ -296,6 +296,7 @@ module Toto
     def title()   self[:title] || "an article"               end
     def date()    @config[:date].call(self[:date])           end
     def author()  self[:author] || @config[:author]          end
+    def category() self[:category] || "category"             end
     def to_html() self.load; super(:article, @config)        end
     alias :to_s to_html
   end
